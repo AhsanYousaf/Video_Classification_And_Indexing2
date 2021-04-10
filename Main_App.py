f1a6b24a73855a7e122fe49c594647eccd21445e
Ahsan Yousaf <ahsanbhatti624@gmail.com>
2021-04-10 23:51:15 +0500

    Changes

--- Main_App.py
+++ Main_App.py
@@ -1,8 +1,54 @@
+import os
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
+from os import system
+import cv2
 import pymysql
 
 root=Tk()
+upload= Tk()
+
+
+def spliting(filename):
+    cam = cv2.VideoCapture(filename)
+
+    try:
+
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
+        # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while (True):
+
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
+
+
 class Main_App:
     def __init__(self, root):
         self.root = root
@@ -15,32 +61,31 @@ class Main_App:
         Frame.txt_name.place(x=300, y=12)
         btn_search = Button(root, text='search', width=8, bg='black', fg='white', command=self.search).place(x=430, y=9)
         btn_upload =Button(root, text='upload', width=8, bg='black', fg='white', command=self.upload).place(x=230, y=9)
-        btn_logout = Button(root, text='Logout', width=8, bg='black', fg='white', command=self.logout).place(x=5, y=9)
+        btn_logout =Button(root, text='Logout', width=8, bg='black', fg='white', command=self.logout).place(x=5, y=9)
         video_box = Frame(root, width=400, highlightbackground='white', height='300').place(x='50', y='100')
         btn_next = Button(root, text='>', width=4, bg='black', fg='white', command=self.next).place(x=250, y=450)
         btn_back = Button(root, text='<', width=4, bg='black', fg='white', command=self.back).place(x=210, y=450)
-
+#to search the user required category
     def search(self):
         messagebox.showinfo("search", "we are searching here", parent=self.root)
+
+
     def upload(self):
+        filename = filedialog.askopenfilename(initialdir="/", title="select a file",
+                                              filetype=(("mp4", "*.mp4"), ("All Files", "*.*")))
+        if(filename!=""):
+            spliting(filename)
+            try:
+                    connection = pymysql.connect(host="localhost", user="root", password="", database="db_connectivity")
+                    cursor = connection.cursor()
+                    cursor.execute("insert into video_db (video) values (%s)",(filename))
+                    connection.commit()
+                    connection.close()
+                    messagebox.showinfo("Success", "Successfuly upload", parent=upload)
+                    # system('Main_App.py')
+            except Exception as es:
+                    messagebox.showerror("Error", f"Error due to:{str(es)}", parent=upload)
 
-        upload= Tk()
-        upload.geometry("400x200")
-        upload.resizable(False, False)
-        upload.configure(background="black")
-        upload.title("upload")
-        btn_bar = Frame(upload, width=150, highlightbackground='white', height='100').place(x='20', y='10')
-        title = Label(upload, text="Title", width=20, font=("bold", 10), bg="black", fg='grey')
-        title.place(x=200, y=40)
-        self.txt_name = Entry(upload).place(x=200, y=40)
-
-        btn_browse = Button(upload, text='Browse', width=8, bg='grey', fg='black', command=self.up_done).place(x=200, y=80)
-        btn_Uplaod = Button(upload, text='upload', width=8, bg='grey', fg='black', command=self.browse).place(x=60, y=130)
-    def up_done(self):
-        messagebox.showinfo("upload", "upload sucessfuly", parent=self.root)
-
-    def browse(self):
-        messagebox.showinfo("browse", "Browsing", parent=self.root)
     def logout(self):
          messagebox.showinfo("logout", "doing logout", parent=self.root)
     def next(self):
